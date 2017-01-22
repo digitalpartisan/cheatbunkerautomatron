c9dfe2a5262445f40d25d88b2c64bcba4f778e04
digitalpartisan <digital.partisan.86@gmail.com>
2017-01-21 16:41:45 -0800

    Added packed files and .bat utilities

---
+++ up-loose.bat
@@ -0,0 +1,2 @@
+mklink /j "K:\Steam\steamapps\common\Fallout 4\Data\Scripts\CheatBunkerDLC01" "K:\FO4 Mods\CheatBunkerAutomatron\Data\Scripts\CheatBunkerDLC01"
+mklink /j "K:\Steam\steamapps\common\Fallout 4\Data\Scripts\Source\User\CheatBunkerDLC01" "K:\FO4 Mods\CheatBunkerAutomatron\Data\Scripts\Source\User\CheatBunkerDLC01"
---
+++ up.bat
@@ -0,0 +1,1 @@
+mklink /h "K:\Steam\steamapps\common\Fallout 4\Data\CheatBunkerAutomatron.esp" "K:\FO4 Mods\CheatBunkerAutomatron\Data\CheatBunkerAutomatron.esp"
