699fd01ef584325090697565f00ff876ec2f43c7
digitalpartisan <digital.partisan.86@gmail.com>
2018-09-28 18:51:14 -0700

    Converted to scaffold

--- up-loose.bat
+++
@@ -1,2 +0,0 @@
-mklink /j "K:\Steam\steamapps\common\Fallout 4\Data\Scripts\CheatBunkerDLC01" "K:\FO4 Mods\CheatBunkerAutomatron\Data\Scripts\CheatBunkerDLC01"
-mklink /j "K:\Steam\steamapps\common\Fallout 4\Data\Scripts\Source\User\CheatBunkerDLC01" "K:\FO4 Mods\CheatBunkerAutomatron\Data\Scripts\Source\User\CheatBunkerDLC01"
--- up.bat
+++
@@ -1,1 +0,0 @@
-mklink /h "K:\Steam\steamapps\common\Fallout 4\Data\CheatBunkerAutomatron.esp" "K:\FO4 Mods\CheatBunkerAutomatron\Data\CheatBunkerAutomatron.esp"
